fde5b69a35eaa98c3f5e39b6610a9c5bbb9e6fa3
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2022-05-06 17:47:04 -0300

    adiciona mensagem de turno

--- PlayerInterface.py
+++ PlayerInterface.py
@@ -23,16 +23,27 @@ class PlayerInterface:
     self.red_piece = PhotoImage(file="images/red_piece.png")
 
     self.reserve_frame_player1 = Frame(self.main_window)
-    self.reserve_frame_player1.grid(row=1, column=0)
+    self.reserve_frame_player1.grid(row=0, column=0)
     self.reserve_text1 = Label(self.reserve_frame_player1, text=6, font="Arial 30", image=self.blue_piece, compound="center", padx=25)
     self.reserve_text1.grid(row=0, column=0)
 
+    self.reserve_frame_player2 = Frame(self.main_window, bg="yellow")
+    self.reserve_frame_player2.grid(row=0, column=2)
+    self.reserve_text2 = Label(self.reserve_frame_player2, text=6, font="Arial 30", image=self.red_piece, compound="center", padx=25)
+    self.reserve_text2.grid(row=0, column=0)
+
+    self.message_frame = Frame(self.main_window)
+    self.message_frame.grid(row=1, column=1)
+    self.message = Label(self.message_frame, text="Turno do jogador 1", font="Arial, 15")
+    self.message.grid(row=0, column=1)
+
     self.table_frame = Frame(self.main_window, pady=50)
-    self.table_frame.grid(row=1, column=1)
+    self.table_frame.grid(row=0, column=1)
     self.disable_positions = [(0, 0), (0,1), (1,0), (0,3), (0,4), (1,4), (3,0), (4,0), (4,1), (4,3), (4,4), (3,4)]
     self.positions_red_pieces = [(2, 0), (2, 1), (1, 1), (1, 2)]
     self.positions_blue_pieces = [(2, 4), (2, 3), (3, 3), (3, 2)]
     self.board_view: list[list[Label]] = []
+
     for x in range(5):
       line = []
       for y in range(5):
@@ -50,10 +61,6 @@ class PlayerInterface:
         place_label.bind("<Button-1>", lambda event, place_line=x, place_column=y:self.click(event, place_line, place_column))
         line.append(place_label)
       self.board_view.append(line)
-    self.reserve_frame_player2 = Frame(self.main_window, bg="yellow")
-    self.reserve_frame_player2.grid(row=1, column=2)
-    self.reserve_text2 = Label(self.reserve_frame_player2, text=6, font="Arial 30", image=self.red_piece, compound="center", padx=25)
-    self.reserve_text2.grid(row=0, column=0)
   
   def click(self, event, x, y):
     print(self.board_view[x][y]['imag'])
@@ -65,4 +72,10 @@ class PlayerInterface:
         image = self.blue_piece_board
       self.board_view[x][y].configure(image=image)
       self.turn = not self.turn
+      self.changeMessage()
 
+  def changeMessage(self):
+    if (self.turn):
+      self.message.configure(text="Turno do jogador 1")
+    else:
+      self.message.configure(text="Turno do jogador 2")
