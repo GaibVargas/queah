fc58446ea8b62ef9ddf41a2d934f56b5c1d76e26
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2022-05-06 16:44:50 -0300

    colocando pecas no tabuleiro

--- PlayerInterface.py
+++ PlayerInterface.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from tkinter import *
 
 WINDOW_WIDTH = 1024
@@ -23,12 +24,21 @@ class PlayerInterface:
     self.table_frame = Frame(self.main_window, pady=50)
     self.table_frame.grid(row=1, column=1)
     self.disable_board_place_image = PhotoImage(file="images/disable_board_place.png")
+    self.board_place_image = PhotoImage(file="images/board_place.png")
+    self.disable_positions = [(0, 0), (0,1), (1,0), (0,3), (0,4), (1,4), (3,0), (4,0), (4,1), (4,3), (4,4), (3,4)]
+    self.board_view = []
     for x in range(5):
+      line = []
       for y in range(5):
-        place_label = Label(self.table_frame, bd=0, image=self.disable_board_place_image)
+        place_label = Label(self.table_frame, bd=0)
+        if ((x,y) in self.disable_positions):
+          place_label.configure(image=self.disable_board_place_image)
+        else:
+          place_label.configure(image=self.board_place_image)
         place_label.grid(row=x, column=y)
         place_label.bind("<Button-1>", lambda event, place_line=x, place_column=y:self.click(event, place_line, place_column))
-
+        line.append(place_label)
+      self.board_view.append(line)
     self.red_piece = PhotoImage(file="images/red_piece.png")
     self.reserve_frame_player2 = Frame(self.main_window, bg="yellow")
     self.reserve_frame_player2.grid(row=1, column=2)
@@ -36,4 +46,5 @@ class PlayerInterface:
     self.reserve_text2.grid(row=0, column=0)
   
   def click(self, event, x, y):
+    self.board_view[x][y].configure(image=self.red_piece)
     print(f"Click: {x}x{y}")
