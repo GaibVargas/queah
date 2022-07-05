b14ba1396bd0e335669b25b06dfdde9389a617c5
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2022-07-05 10:07:11 -0300

    traduzindo frames e labels

--- PlayerInterface.py
+++ PlayerInterface.py
@@ -1,81 +1,100 @@
 from tkinter import *
+from Jogador import Jogador
+
+from Tabuleiro import Tabuleiro
 
 WINDOW_WIDTH = 746
 WINDOW_HEIGHT = 650
 
 class PlayerInterface:
   def __init__(self):
-    self.main_window = Tk()
+    self.janela_principal = Tk()
+    self.tabuleiro = Tabuleiro(Jogador(), Jogador())
     self.fill_main_window()
-    self.main_window.mainloop()
+    self.janela_principal.mainloop()
 
   def fill_main_window(self):
-    self.main_window.title("Queah")
-    self.main_window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-    self.main_window.resizable(False, False)
+    self.janela_principal.title("Queah")
+    self.janela_principal.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+    self.janela_principal.resizable(False, False)
     self.turn = True
 
-    self.disable_board_place_image = PhotoImage(file="images/disable_board_place.png")
-    self.board_place_image = PhotoImage(file="images/board_place.png")
-    self.blue_piece_board = PhotoImage(file="images/blue_piece_board.png")
-    self.red_piece_board = PhotoImage(file="images/red_piece_board.png")
-    self.blue_piece = PhotoImage(file="images/blue_piece.png")
-    self.red_piece = PhotoImage(file="images/red_piece.png")
+    self.imagem_posicao_desabilitada = PhotoImage(file="images/disable_board_place.png")
+    self.imagem_posicao = PhotoImage(file="images/board_place.png")
+    self.imagem_peca_azul_tabuleiro = PhotoImage(file="images/blue_piece_board.png")
+    self.imagem_peca_vermelha_tabuleiro = PhotoImage(file="images/red_piece_board.png")
+    self.imagem_peca_azul = PhotoImage(file="images/blue_piece.png")
+    self.imagem_peca_vermelha = PhotoImage(file="images/red_piece.png")
 
-    self.reserve_frame_player1 = Frame(self.main_window)
-    self.reserve_frame_player1.grid(row=0, column=0)
-    self.reserve_text1 = Label(self.reserve_frame_player1, text=6, font="Arial 30", image=self.blue_piece, compound="center", padx=20)
-    self.reserve_text1.grid(row=0, column=0)
+    self.reserva_jogador1 = Frame(self.janela_principal)
+    self.reserva_jogador1.grid(row=0, column=0)
+    self.reserva_jogador1_texto = Label(self.reserva_jogador1, text=6, font="Arial 30", image=self.imagem_peca_azul, compound="center", padx=20)
+    self.reserva_jogador1_texto.grid(row=0, column=0)
 
-    self.reserve_frame_player2 = Frame(self.main_window, bg="yellow")
-    self.reserve_frame_player2.grid(row=0, column=2)
-    self.reserve_text2 = Label(self.reserve_frame_player2, text=6, font="Arial 30", image=self.red_piece, compound="center", padx=20)
-    self.reserve_text2.grid(row=0, column=0)
+    self.reserva_jogador2 = Frame(self.janela_principal, bg="yellow")
+    self.reserva_jogador2.grid(row=0, column=2)
+    self.reserva_jogador2_texto = Label(self.reserva_jogador2, text=6, font="Arial 30", image=self.imagem_peca_vermelha, compound="center", padx=20)
+    self.reserva_jogador2_texto.grid(row=0, column=0)
 
-    self.message_frame = Frame(self.main_window)
-    self.message_frame.grid(row=1, column=1)
-    self.message = Label(self.message_frame, text="Turno do jogador 1", font="Arial, 15")
-    self.message.grid(row=0, column=1)
+    self.mensagem_frame = Frame(self.janela_principal)
+    self.mensagem_frame.grid(row=1, column=1)
+    self.mensagem = Label(self.mensagem_frame, text="Turno do jogador 1", font="Arial, 15")
+    self.mensagem.grid(row=0, column=1)
 
-    self.table_frame = Frame(self.main_window, pady=50)
-    self.table_frame.grid(row=0, column=1)
-    self.disable_positions = [(0, 0), (0,1), (1,0), (0,3), (0,4), (1,4), (3,0), (4,0), (4,1), (4,3), (4,4), (3,4)]
-    self.positions_red_pieces = [(2, 0), (2, 1), (1, 1), (1, 2)]
-    self.positions_blue_pieces = [(2, 4), (2, 3), (3, 3), (3, 2)]
-    self.board_view: list[list[Label]] = []
+    self.tabuleiro_frame = Frame(self.janela_principal, pady=50)
+    self.tabuleiro_frame.grid(row=0, column=1)
+    self.posicoes_desabilitadas = [(0, 0), (0,1), (1,0), (0,3), (0,4), (1,4), (3,0), (4,0), (4,1), (4,3), (4,4), (3,4)]
+    self.posicoes_iniciais_pecas_vermelhas = [(2, 0), (2, 1), (1, 1), (1, 2)]
+    self.posicoes_iniciais_pecas_azuis = [(2, 4), (2, 3), (3, 3), (3, 2)]
+    self.posicoes: list[list[Label]] = []
 
     for x in range(5):
       line = []
       for y in range(5):
-        place_label = Label(self.table_frame, bd=0)
-        if ((x,y) in self.disable_positions):
-          place_label.configure(image=self.disable_board_place_image)
+        place_label = Label(self.tabuleiro_frame, bd=0)
+        if ((x,y) in self.posicoes_desabilitadas):
+          place_label.configure(image=self.imagem_posicao_desabilitada)
         else:
-          if ((x,y) in self.positions_red_pieces):
-            place_label.configure(image=self.red_piece_board)
-          elif ((x,y) in self.positions_blue_pieces):
-            place_label.configure(image=self.blue_piece_board)
+          if ((x,y) in self.posicoes_iniciais_pecas_vermelhas):
+            place_label.configure(image=self.imagem_peca_vermelha_tabuleiro)
+          elif ((x,y) in self.posicoes_iniciais_pecas_azuis):
+            place_label.configure(image=self.imagem_peca_azul_tabuleiro)
           else:
-            place_label.configure(image=self.board_place_image)
+            place_label.configure(image=self.imagem_posicao)
         place_label.grid(row=x, column=y)
         place_label.bind("<Button-1>", lambda event, place_line=x, place_column=y:self.click(event, place_line, place_column))
         line.append(place_label)
-      self.board_view.append(line)
+      self.posicoes.append(line)
   
+  def clickIniciar(self):
+    pass
+
+  def iniciarGUIJogo(self):
+    pass
+
+  def clickReiniciar(self):
+    pass
+
+  def atualizarGUI(self):
+    pass
+
+  def clickPosicao(self):
+    pass
+
   def click(self, event, x, y):
-    print(self.board_view[x][y]['imag'])
-    if ((x,y) not in self.disable_positions):
+    print(self.posicoes[x][y]['imag'])
+    if ((x,y) not in self.posicoes_desabilitadas):
       image: PhotoImage
       if (self.turn):
-        image = self.red_piece_board
+        image = self.imagem_peca_vermelha_tabuleiro
       else:
-        image = self.blue_piece_board
-      self.board_view[x][y].configure(image=image)
+        image = self.imagem_peca_azul_tabuleiro
+      self.posicoes[x][y].configure(image=image)
       self.turn = not self.turn
       self.changeMessage()
 
   def changeMessage(self):
     if (self.turn):
-      self.message.configure(text="Turno do jogador 1")
+      self.mensagem.configure(text="Turno do jogador 1")
     else:
-      self.message.configure(text="Turno do jogador 2")
+      self.mensagem.configure(text="Turno do jogador 2")
